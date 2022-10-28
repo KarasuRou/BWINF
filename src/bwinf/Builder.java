3a67973fb96c963edb7156dbdb5017012efa1672
KarasuRou <Tjalf0000@gmx.de>
2022-10-28 17:07:16 +0200

    Initial Commit

---
+++ src/bwinf/Builder.java
@@ -0,0 +1,12 @@
+package bwinf;
+
+public class Builder {
+
+    public static void main(String[] args) {
+//        readConfig();
+//
+//        buildPicture(config);
+//
+//        savePicture(picture);
+    }
+}
